68b9f573fb3152ca78f15f9031f28999b0c69916
Chayan <chayan.eee.bsmrstu.bd@gmail.com>
2021-10-27 22:30:58 +0600

    X2: added DRM for videoplayback fix in tweeter, insta etc.

--- device.mk
+++ device.mk
@@ -81,6 +81,15 @@ PRODUCT_PACKAGES += \
 # RealmeParts
 $(call inherit-product, device/realme/X2/RealmeParts/parts.mk)    
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm@1.0 \
+    android.hardware.drm@1.1 \
+    android.hardware.drm@1.2 \
+    android.hardware.drm@1.3
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.x2 \
