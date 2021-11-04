84d18358c93fcc87c844ec9db491cb6c867e86aa
chaitanya <chaitanyaarora3@gmail.com>
2021-11-04 15:06:50 +0530

    X2:OCTAVI BRINGUP

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,10 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_X2.mk
+    $(LOCAL_DIR)/octavi_X2.mk
+
+
+COMMON_LUNCH_CHOICES := \
+    octavi_X2-eng \
+    octavi_X2-user \
+    octavi_X2-userdebug
