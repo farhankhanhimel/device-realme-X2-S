9498df41b8b5a8b2e3ccaa386e58ffc4e53b9bed
farhankhanhimel <93470433+farhankhanhimel@users.noreply.github.com>
2021-12-08 00:13:55 +0600

    Update extract-files.sh

--- extract-files.sh
+++ extract-files.sh
@@ -14,9 +14,9 @@ VENDOR=realme
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-FLUID_ROOT="${MY_DIR}"/../../..
+SUPERIOR_ROOT="${MY_DIR}"/../../..
 
-HELPER="${FLUID_ROOT}/vendor/fluid/build/tools/extract_utils.sh"
+HELPER="${SUPERIOR_ROOT}/vendor/superior/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -50,7 +50,7 @@ if [ -z "${SRC}" ]; then
 fi
 
 # Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${FLUID_ROOT}" true "${CLEAN_VENDOR}"
+setup_vendor "${DEVICE}" "${VENDOR}" "${SUPERIOR_ROOT}" true "${CLEAN_VENDOR}"
 
 extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
         "${KANG}" --section "${SECTION}"
