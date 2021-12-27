a6c23b1323a249eb027aeced8088030f7191b13c
farhankhanhimel <93470433+farhankhanhimel@users.noreply.github.com>
2021-12-27 17:40:13 +0600

    Update nad_X2.mk

--- nad_X2.mk
+++ nad_X2.mk
@@ -20,7 +20,7 @@ TARGET_FOD_ANIMATIONS := true
 
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := nusantara_X2
+PRODUCT_NAME := nad_X2
 PRODUCT_DEVICE := X2
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme X2
